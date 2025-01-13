6df3249fce84a43fa397f35220adeff7d30db376
Sangwanji <sakshamsangwan2005@outlook.com>
2025-01-13 23:06:39 +0530

    Sorting I complete

---
+++ Sorting/insertionsort.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+void m1(int arr[],int n){
+    for(int i=1;i<n;i++){
+        int curr = arr[i];
+        int prev = i-1;
+        while(prev>=0 && arr[prev]>curr){
+            arr[prev+1] = arr[prev];
+            prev--;
+        }
+        arr[prev+1] = curr;
+    }
+}
+
+void m2(int arr[],int n){
+    for(int i=0;i<n;i++){
+        int j=i;
+        while(j>0 && arr[j-1]>arr[j]){
+            swap(arr[j],arr[j-1]);
+            j--;
+        }
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++) cin>>arr[i];
+    m2(arr,n);
+    for(int j: arr) cout<<j<<" ";
+}
